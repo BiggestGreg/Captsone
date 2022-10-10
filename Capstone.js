44e3446d75f0bc9678c7cd0da3f669fc99cd6cdb
BiggestGreg <108503103+BiggestGreg@users.noreply.github.com>
2022-10-09 19:26:10 -0600

    Add files via upload

---
+++ Capstone.js
@@ -0,0 +1,22 @@
+const noob = document.getElementById('EA3');
+
+noob.addEventListener('click', function onClick() {
+    noob.style.backgroundColor = 'red'
+});
+
+const red = document.getElementById('EA1');
+
+function onClick () {
+    
+}
+
+
+red.addEventListener('click', function onClick() {
+    if (red.style.backgroundColor = 'blue') {        
+        red.style.backgroundColor = 'red'
+    }
+    else {
+        red.style.backgroundColor = 'gray'
+    }
+
+});
---
+++ Setup.js
@@ -0,0 +1,14 @@
+let Begin = document.querySelector("#startG");
+
+Begin.addEventListener('click', Start);
+
+function Start () {
+
+}
+
+
+const noob = document.getElementsByClassName('setUpO');
+
+noob.addEventListener('click', function onClick() {
+    noob.style.backgroundColor = 'red'
+});
