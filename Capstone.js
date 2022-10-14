835891a8cd3386232c0e562220ddc3374370c58b
BiggestGreg <108503103+BiggestGreg@users.noreply.github.com>
2022-10-13 21:39:52 -0700

    Add files via upload

---
+++ Capstone.js
@@ -0,0 +1,628 @@
+
+
+const ea1 = document.getElementById('EA1');
+const ea2 = document.getElementById('EA2');
+const ea3 = document.getElementById('EA3');
+const ea4 = document.getElementById('EA4');
+const ea5 = document.getElementById('EA5');
+const ea6 = document.getElementById('EA6');
+const eb1 = document.getElementById('EB1');
+const eb2 = document.getElementById('EB2');
+const eb3 = document.getElementById('EB3');
+const eb4 = document.getElementById('EB4');
+const eb5 = document.getElementById('EB5');
+const eb6 = document.getElementById('EB6');
+const ec1 = document.getElementById('EC1');
+const ec2 = document.getElementById('EC2');
+const ec3 = document.getElementById('EC3');
+const ec4 = document.getElementById('EC4');
+const ec5 = document.getElementById('EC5');
+const ec6 = document.getElementById('EC6');
+const ed1 = document.getElementById('ED1');
+const ed2 = document.getElementById('ED2');
+const ed3 = document.getElementById('ED3');
+const ed4 = document.getElementById('ED4');
+const ed5 = document.getElementById('ED5');
+const ed6 = document.getElementById('ED6');
+const ee1 = document.getElementById('EE1');
+const ee2 = document.getElementById('EE2');
+const ee3 = document.getElementById('EE3');
+const ee4 = document.getElementById('EE4');
+const ee5 = document.getElementById('EE5');
+const ee6 = document.getElementById('EE6');
+const ef1 = document.getElementById('EF1');
+const ef2 = document.getElementById('EF2');
+const ef3 = document.getElementById('EF3');
+const ef4 = document.getElementById('EF4');
+const ef5 = document.getElementById('EF5');
+const ef6 = document.getElementById('EF6');
+
+
+
+
+
+
+
+function bea1() {
+    ea1.style.backgroundColor = 'red'
+    
+    if (ga1.style.backgroundColor === 'gray') {
+        ga1.style.backgroundColor = 'red'
+    }
+    else {
+        ga1.style.backgroundColor = 'white'
+    }
+}
+
+function bea2() {
+    ea2.style.backgroundColor = 'white'
+    
+    if (gd6.style.backgroundColor === 'gray') {
+        gd6.style.backgroundColor = 'red'
+    }
+    else {
+        gd6.style.backgroundColor = 'white'
+    }
+}
+
+function bea3() {
+    ea3.style.backgroundColor = 'white'
+    
+    if (gb3.style.backgroundColor === 'gray') {
+        gb3.style.backgroundColor = 'red'
+    }
+    else {
+        gb3.style.backgroundColor = 'white'
+    }
+}
+
+function bea4() {
+    ea4.style.backgroundColor = 'white'
+    
+    if (gf4.style.backgroundColor === 'gray') {
+        gf4.style.backgroundColor = 'red'
+    }
+    else {
+        gf4.style.backgroundColor = 'white'
+    }
+}
+
+function bea5() {
+    ea5.style.backgroundColor = 'white'
+    
+    if (gc6.style.backgroundColor === 'gray') {
+        gc6.style.backgroundColor = 'red'
+    }
+    else {
+        gc6.style.backgroundColor = 'white'
+    }
+}
+
+function bea6() {
+    ea6.style.backgroundColor = 'white'
+    
+    if (ga3.style.backgroundColor === 'gray') {
+        ga3.style.backgroundColor = 'red'
+    }
+    else {
+        ga3.style.backgroundColor = 'white'
+    }
+}
+
+function beb1() {
+    eb1.style.backgroundColor = 'red'
+    
+    if (ge5.style.backgroundColor === 'gray') {
+        ge5.style.backgroundColor = 'red'
+    }
+    else {
+        ge5.style.backgroundColor = 'white'
+    }
+}
+
+function beb2() {
+    eb2.style.backgroundColor = 'white'
+    
+    if (gd1.style.backgroundColor === 'gray') {
+        gd1.style.backgroundColor = 'red'
+    }
+    else {
+        gd1.style.backgroundColor = 'white'
+    }
+}
+
+function beb3() {
+    eb3.style.backgroundColor = 'white'
+    
+    if (ga5.style.backgroundColor === 'gray') {
+        ga5.style.backgroundColor = 'red'
+    }
+    else {
+        ga5.style.backgroundColor = 'white'
+    }
+}
+
+function beb4() {
+    eb4.style.backgroundColor = 'white'
+    
+    if (gd3.style.backgroundColor === 'gray') {
+        gd3.style.backgroundColor = 'red'
+    }
+    else {
+        gd3.style.backgroundColor = 'white'
+    }
+}
+
+function beb5() {
+    eb5.style.backgroundColor = 'red'
+    
+    if (gf3.style.backgroundColor === 'gray') {
+        gf3.style.backgroundColor = 'red'
+    }
+    else {
+        gf3.style.backgroundColor = 'white'
+    }
+}
+
+function beb6() {
+    eb6.style.backgroundColor = 'white'
+    
+    if (ga4.style.backgroundColor === 'gray') {
+        ga4.style.backgroundColor = 'red'
+    }
+    else {
+        ga4.style.backgroundColor = 'white'
+    }
+}
+
+function bec1() {
+    ec1.style.backgroundColor = 'red'
+    
+    if (gb6.style.backgroundColor === 'gray') {
+        gb6.style.backgroundColor = 'red'
+    }
+    else {
+        gb6.style.backgroundColor = 'white'
+    }
+}
+
+function bec2() {
+    ec2.style.backgroundColor = 'white'
+    
+    if (gc3.style.backgroundColor === 'gray') {
+        gc3.style.backgroundColor = 'red'
+    }
+    else {
+        gc3.style.backgroundColor = 'white'
+    }
+}
+
+function bec3() {
+    ec3.style.backgroundColor = 'white'
+    
+    if (ga2.style.backgroundColor === 'gray') {
+        ga2.style.backgroundColor = 'red'
+    }
+    else {
+        ga2.style.backgroundColor = 'white'
+    }
+}
+
+function bec4() {
+    ec4.style.backgroundColor = 'white'
+    
+    if (gd4.style.backgroundColor === 'gray') {
+        gd4.style.backgroundColor = 'red'
+    }
+    else {
+        gd4.style.backgroundColor = 'white'
+    }
+}
+
+function bec5() {
+    ec5.style.backgroundColor = 'red'
+    
+    if (gb2.style.backgroundColor === 'gray') {
+        gb2.style.backgroundColor = 'red'
+    }
+    else {
+        gb2.style.backgroundColor = 'white'
+    }
+}
+
+function bec6() {
+    ec6.style.backgroundColor = 'white'
+    
+    if (ge3.style.backgroundColor === 'gray') {
+        ge3.style.backgroundColor = 'red'
+    }
+    else {
+        ge3.style.backgroundColor = 'white'
+    }
+}
+
+function bed1() {
+    ed1.style.backgroundColor = 'white'
+    
+    if (ga6.style.backgroundColor === 'gray') {
+        ga6.style.backgroundColor = 'red'
+    }
+    else {
+        ga6.style.backgroundColor = 'white'
+    }
+}
+
+function bed2() {
+    ed2.style.backgroundColor = 'white'
+    
+    if (gf1.style.backgroundColor === 'gray') {
+        gf1.style.backgroundColor = 'red'
+    }
+    else {
+        gf1.style.backgroundColor = 'white'
+    }
+}
+
+function bed3() {
+    ed3.style.backgroundColor = 'white'
+    
+    if (gd2.style.backgroundColor === 'gray') {
+        gd2.style.backgroundColor = 'red'
+    }
+    else {
+        gd2.style.backgroundColor = 'white'
+    }
+}
+
+function bed4() {
+    ed4.style.backgroundColor = 'white'
+    
+    if (ge1.style.backgroundColor === 'gray') {
+        ge1.style.backgroundColor = 'red'
+    }
+    else {
+        ge1.style.backgroundColor = 'white'
+    }
+}
+
+function bed5() {
+    ed5.style.backgroundColor = 'white'
+    
+    if (ge2.style.backgroundColor === 'gray') {
+        ge2.style.backgroundColor = 'red'
+    }
+    else {
+        ge2.style.backgroundColor = 'white'
+    }
+}
+
+function bed6() {
+    ed6.style.backgroundColor = 'white'
+    
+    if (gb4.style.backgroundColor === 'gray') {
+        gb4.style.backgroundColor = 'red'
+    }
+    else {
+        gb4.style.backgroundColor = 'white'
+    }
+}
+
+function bee1() {
+    ee1.style.backgroundColor = 'white'
+    
+    if (gf2.style.backgroundColor === 'gray') {
+        gf2.style.backgroundColor = 'red'
+    }
+    else {
+        gf2.style.backgroundColor = 'white'
+    }
+}
+
+function bee2() {
+    ee2.style.backgroundColor = 'red'
+    
+    if (ge4.style.backgroundColor === 'gray') {
+        ge4.style.backgroundColor = 'red'
+    }
+    else {
+        ge4.style.backgroundColor = 'white'
+    }
+}
+
+function bee3() {
+    ee3.style.backgroundColor = 'red'
+    
+    if (gc2.style.backgroundColor === 'gray') {
+        gc2.style.backgroundColor = 'red'
+    }
+    else {
+        gc2.style.backgroundColor = 'white'
+    }
+}
+
+function bee4() {
+    ee4.style.backgroundColor = 'white'
+    
+    if (ge6.style.backgroundColor === 'gray') {
+        ge6.style.backgroundColor = 'red'
+    }
+    else {
+        ge6.style.backgroundColor = 'white'
+    }
+}
+
+function bee5() {
+    ee5.style.backgroundColor = 'white'
+    
+    if (gc4.style.backgroundColor === 'gray') {
+        gc4.style.backgroundColor = 'red'
+    }
+    else {
+        gc4.style.backgroundColor = 'white'
+    }
+}
+
+function bee6() {
+    ee6.style.backgroundColor = 'white'
+    
+    if (gd5.style.backgroundColor === 'gray') {
+        gd5.style.backgroundColor = 'red'
+    }
+    else {
+        gd5.style.backgroundColor = 'white'
+    }
+}
+
+function bef1() {
+    ef1.style.backgroundColor = 'white'
+    
+    if (gf6.style.backgroundColor === 'gray') {
+        gf6.style.backgroundColor = 'red'
+    }
+    else {
+        gf6.style.backgroundColor = 'white'
+    }
+}
+
+function bef2() {
+    ef2.style.backgroundColor = 'white'
+    
+    if (gc1.style.backgroundColor === 'gray') {
+        gc1.style.backgroundColor = 'red'
+    }
+    else {
+        gc1.style.backgroundColor = 'white'
+    }
+}
+
+function bef3() {
+    ef3.style.backgroundColor = 'white'
+    
+    if (gb1.style.backgroundColor === 'gray') {
+        gb1.style.backgroundColor = 'red'
+    }
+    else {
+        gb1.style.backgroundColor = 'white'
+    }
+}
+
+function bef4() {
+    ef4.style.backgroundColor = 'white'
+    
+    if (gf5.style.backgroundColor === 'gray') {
+        gf5.style.backgroundColor = 'red'
+    }
+    else {
+        gf5.style.backgroundColor = 'white'
+    }
+}
+
+function bef5() {
+    ef5.style.backgroundColor = 'red'
+    
+    if (gc5.style.backgroundColor === 'gray') {
+        gc5.style.backgroundColor = 'red'
+    }
+    else {
+        gc5.style.backgroundColor = 'white'
+    }
+}
+
+function bef6() {
+    ef6.style.backgroundColor = 'white'
+    
+    if (gb5.style.backgroundColor === 'gray') {
+        gb5.style.backgroundColor = 'red'
+    }
+    else {
+        gb5.style.backgroundColor = 'white'
+    }
+}
+
+
+
+
+const ga1 = document.getElementById('GA1');
+const ga2 = document.getElementById('GA2');
+const ga3 = document.getElementById('GA3');
+const ga4 = document.getElementById('GA4');
+const ga5 = document.getElementById('GA5');
+const ga6 = document.getElementById('GA6');
+const gb1 = document.getElementById('GB1');
+const gb2 = document.getElementById('GB2');
+const gb3 = document.getElementById('GB3');
+const gb4 = document.getElementById('GB4');
+const gb5 = document.getElementById('GB5');
+const gb6 = document.getElementById('GB6');
+const gc1 = document.getElementById('GC1');
+const gc2 = document.getElementById('GC2');
+const gc3 = document.getElementById('GC3');
+const gc4 = document.getElementById('GC4');
+const gc5 = document.getElementById('GC5');
+const gc6 = document.getElementById('GC6');
+const gd1 = document.getElementById('GD1');
+const gd2 = document.getElementById('GD2');
+const gd3 = document.getElementById('GD3');
+const gd4 = document.getElementById('GD4');
+const gd5 = document.getElementById('GD5');
+const gd6 = document.getElementById('GD6');
+const ge1 = document.getElementById('GE1');
+const ge2 = document.getElementById('GE2');
+const ge3 = document.getElementById('GE3');
+const ge4 = document.getElementById('GE4');
+const ge5 = document.getElementById('GE5');
+const ge6 = document.getElementById('GE6');
+const gf1 = document.getElementById('GF1');
+const gf2 = document.getElementById('GF2');
+const gf3 = document.getElementById('GF3');
+const gf4 = document.getElementById('GF4');
+const gf5 = document.getElementById('GF5');
+const gf6 = document.getElementById('GF6');
+
+
+
+
+
+function bga1() {
+    ga1.style.backgroundColor = 'gray'
+}
+
+function bga2() {
+    ga2.style.backgroundColor = 'gray'
+}
+
+function bga3() {
+    ga3.style.backgroundColor = 'gray'
+}
+
+function bga4() {
+    ga4.style.backgroundColor = 'gray'
+}
+
+function bga5() {
+    ga5.style.backgroundColor = 'gray'
+}
+
+function bga6() {
+    ga6.style.backgroundColor = 'gray'
+}
+
+function bgb1() {
+    gb1.style.backgroundColor = 'gray'
+}
+
+function bgb2() {
+    gb2.style.backgroundColor = 'gray'
+}
+
+function bgb3() {
+    gb3.style.backgroundColor = 'gray'
+}
+
+function bgb4() {
+    gb4.style.backgroundColor = 'gray'
+}
+
+function bgb5() {
+    gb5.style.backgroundColor = 'gray'
+}
+
+function bgb6() {
+    gb6.style.backgroundColor = 'gray'
+}
+
+function bgc1() {
+    gc1.style.backgroundColor = 'gray'
+}
+
+function bgc2() {
+    gc2.style.backgroundColor = 'gray'
+}
+
+function bgc3() {
+    gc3.style.backgroundColor = 'gray'
+}
+
+function bgc4() {
+    gc4.style.backgroundColor = 'gray'
+}
+
+function bgc5() {
+    gc5.style.backgroundColor = 'gray'
+}
+
+function bgc6() {
+    gc6.style.backgroundColor = 'gray'
+}
+
+function bgd1() {
+    gd1.style.backgroundColor = 'gray'
+}
+
+function bgd2() {
+    gd3.style.backgroundColor = 'gray'
+}
+
+function bgd3() {
+    gd3.style.backgroundColor = 'gray'
+}
+
+function bgd4() {
+    gd4.style.backgroundColor = 'gray'
+}
+
+function bgd5() {
+    gd5.style.backgroundColor = 'gray'
+}
+
+function bgd6() {
+    gd6.style.backgroundColor = 'gray'
+}
+
+function bge1() {
+    ge1.style.backgroundColor = 'gray'
+}
+
+function bge2() {
+    ge2.style.backgroundColor = 'gray'
+}
+
+function bge3() {
+    ge3.style.backgroundColor = 'gray'
+}
+
+function bge4() {
+    ge4.style.backgroundColor = 'gray'
+}
+
+function bge5() {
+    ge5.style.backgroundColor = 'gray'
+}
+
+function bge6() {
+    ge6.style.backgroundColor = 'gray'
+}
+
+function bgf1() {
+    gf1.style.backgroundColor = 'gray'
+}
+
+function bgf2() {
+    gf2.style.backgroundColor = 'gray'
+}
+
+function bgf3() {
+    gf3.style.backgroundColor = 'gray'
+}
+
+function bgf4() {
+    gf4.style.backgroundColor = 'gray'
+}
+
+function bgf5() {
+    gf5.style.backgroundColor = 'gray'
+}
+
+function bgf6() {
+    gf6.style.backgroundColor = 'gray'
+}
